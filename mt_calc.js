ed7c5e526d9aef29675fe017f20cc52a04c52e9d
Unknown <mjohnson030@west-mec.org>
2019-03-27 18:25:26 -0700

    Commit 3
    Created functions and attached CSS to the HTML so that calculator can properly Display

--- mt_calc.js
+++ mt_calc.js
@@ -35,10 +35,66 @@
 
 
 
+window.onload = init;
+
+function init() {
+      var calcButtons = document.getElementsByClassName("calcButton");
+      for (var i = 0; i < calcButtons.length; i++) {
+            calcButtons[i].onClick = buttonClick; 
+
+      }
+      document.getElementById("calcWindow").onkeydown = calcKeys;
+}
+     
+
+      function buttonClick(e){
+            var calcValue = document.getElementById("calcWindow").value;
+            var calcDecimal = document.getElementById("decimals").value;
+            var buttonValue = e.target.value;
+
+            switch (buttonValue) {
+            case "del":
+                  calcValue = "";
+                  break;
+            case "bksp":
+                  calcValue = eraserChar(calcValue);
+                  break;
+            case "enter":
+                  calcValue  += " = " + evalEq(calcValue, calcDecimal) + "\n";
+                  break;
+            case "prev":
+                  calcValue  += lastEq(calcValue) + "\n";
+                  break;
+            default:
+                  calcValue += buttonValue;
+                  break;
+            }
+            document.getElementById("calcWindow").value = calcValue;
+            document.getElementById("calcWindow").focus();
+      }
+
+      function calcKeys(e) {
+            var calcValue = document.getElementById("calcWindow").value;
+            var calcDecimal = document.getElementById("decimals").value;
+            switch (e.key) {
+                  case "Delete":
+                        calcValue = "";
+                        break;
+                  case "Enter":
+                        calcValue += " = " + evalEq(calcValue, calcDecimal);
+                        break;
+                  case "ArrowUp":
+                        calcValue += lastEq(calcWindow.value);
+                        e.preventDefault();
+            }
+            document.getElementById("calcWindow").value = calcValue;
+      }
 
 
 
 /* ===================================================================== */
+
+
 
 function eraseChar(textStr) { 
    return textStr.substr(0, textStr.length - 1);
@@ -56,20 +112,3 @@ function lastEq(textStr) {
    var lastExp = lines[lines.length-2];
    return lastExp.substr(0, lastExp.indexOf("=")).trim();
 }
-
-window.onload = init;
-
-function init() {
-      var calcButtons = document.querySelectorAll("calcWIndow");
-      for (var i = 0; i =  array.length; i++) {
-            buttonClick();
-
-      }
-
-      function buttonClick(){
-            var calcValue = document.getElementsByTagName("calcWindow");
-            var calcDecimal = 
-      }
-
-     
-}  
